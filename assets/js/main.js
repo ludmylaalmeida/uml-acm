96bab0bcee30a28eb151b62dcb9dc9683d8da2e7
ludmyla almeida <ludmylaalmeida@ludmylas-MacBook-Pro.local>
2020-02-04 18:40:28 -0500

    fixed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,16 +28,18 @@ $(function() {
     var scrollbarLocation = $(this).scrollTop();
 
     scrollLink.each(function() {
-      var sectionOffset = $(scrollLink).offset().top - 73;
+      if (this.hash != "") {
+        var sectionOffset = $(this.hash).offset().top - 73;
 
-      if (sectionOffset <= scrollbarLocation) {
-        $(this)
-          .parent()
-          .addClass("active");
-        $(this)
-          .parent()
-          .siblings()
-          .removeClass("active");
+        if (sectionOffset <= scrollbarLocation) {
+          $(this)
+            .parent()
+            .addClass("active");
+          $(this)
+            .parent()
+            .siblings()
+            .removeClass("active");
+        }
       }
     });
   });
